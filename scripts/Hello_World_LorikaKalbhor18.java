8569dcffdd9d2f9ebf4d1b7d5a357045bd048b8e
Lorika Kalbhor <66677440+LorikaKalbhor18@users.noreply.github.com>
2021-10-07 11:08:24 +0530

    Created Script filr

---
+++ scripts/Hello_World_LorikaKalbhor18.java
@@ -0,0 +1,9 @@
+// LANGUAGE: Java
+// ENV: JRE
+// AUTHOR: Lorika Kalbhor
+// GITHUB: https://github.com/LorikaKalbhor18
+public class Hello_World_LorikaKalbhor18{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        }
+        }
