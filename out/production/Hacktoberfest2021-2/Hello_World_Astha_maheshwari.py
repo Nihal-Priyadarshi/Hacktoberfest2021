5bb3e11ba19f426499b0d1bc8eef8c104d4e92fe
danushan99 <danuthasan08@gmail.com>
2021-10-11 01:42:40 +0530

    add my java program

---
+++ out/production/Hacktoberfest2021-2/Hello_World_Astha_maheshwari.py
@@ -0,0 +1,4 @@
+import sys
+
+print("Hello World")
+print("Hey, I am Astha and I'm contributing to open sourse projects for the first time")
---
+++ out/production/Hacktoberfest2021-2/Hello_World_web-codegrammer.js
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+
+<body>
+
+  <p>Before the script...</p>
+
+<script>
+// using document.write
+document.write('Hello World');
+</script>
+
+  <p>...After the script.</p>
+
+</body>
+
+</html>
